56cc75f133cb7b0ec91afe09f14b4baf59f0dc13
michaelthierry <michaelthierry86@gmail.com>
2024-09-12 06:34:34 -0400

    up project

---
+++ script.js
@@ -0,0 +1,208 @@
+
+const perguntas = document.querySelector(".pergunta");
+const opcoes = document.querySelector(".opcoes");
+const quantidadeOpcao = document.querySelector(".opcoes");
+const animacao = document.querySelector(".animation");
+const fundo = document.querySelector("#body");
+const form = document.querySelector("#form-id");
+const areOpcao = document.getElementById('areaOpcao');
+
+
+const images = [
+    'img/img01.jpg',
+    'img/img02.jpg',
+    'img/img03.jpg',
+    'img/img04.jpg',
+    'img/img05.jpg',
+    'img/img06.jpg',
+    'img/img07.jpg',
+    'img/img08.jpg',
+    'img/img09.jpg',
+    'img/img10.jpg',
+];
+
+let indiceQuestao = 0;
+let clickedButtonValue = '';
+var index=1;
+let botao_clicado = [];
+
+import questions from './scripts/questions.js';
+
+// adiciona a função carregar questão ao botao iniciar
+document.getElementById('iniciar').addEventListener('click', carregaQuestao);
+
+// Exibe mensagem no fim da pesquisa
+function fimPesquisa(){
+
+    console.log("enviado");
+    opcoes.innerHTML = '';
+    perguntas.innerHTML = '';
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <h2>
+            Obrigado! Vai pela sombra, viu?. Olha os carros 
+        </h2>
+    `;
+    
+    opcoes.appendChild(div);
+    deletButtonsNavegate();
+    setTimeout(() => {
+        location.reload(); // Recarrega a página após 2 segundos
+    }, 5000);
+}
+// carrega a proxima questão
+function proximaQuestao(e){
+    // verifica se o indice é menor quantidade de questões 
+    if(indiceQuestao < questions.length - 1){
+        // incrementa o indice
+        indiceQuestao++;
+        botao_clicado.push(e);
+        // chama a função para carregar questão seguinte
+        carregaQuestao();
+        console.log("valor Index "+`${e}`);
+    }else{
+        console.log("fim");
+    }
+    
+}
+// carrega a questão anterior
+function anteriorQuestao(e){
+    // verifica se o indie
+    if(indiceQuestao > 0){
+        indiceQuestao--;
+        index--;
+        carregaQuestao();
+    }else{
+        console.log("primeira questão");
+    }
+}
+// cria botão de enviar resposta
+function enviarResposta(e){
+    // adiciona a opção vetor
+    botao_clicado.push(e);
+    opcoes.innerHTML = '';
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <button type="submit" class="resposta">
+           Enviar
+        </button>
+    `;
+    console.log("valor Index "+`${e}`);
+    div.addEventListener('click', (evento) => {
+        evento.preventDefault();
+        const formData = new FormData(form);
+        const data = Object.fromEntries(formData);
+        data.botao_clicado = botao_clicado;
+        const jsonData = JSON.stringify(data);
+        console.log(jsonData);
+        fimPesquisa();
+    });
+    opcoes.appendChild(div);
+}
+// carrega questões na tela de questões
+function carregaQuestao(){
+    // verifica se o bota iniciar chamou e o remove
+    const div = document.getElementById('iniciar');
+    if(div){
+        div.remove();
+    }
+    
+    // pega a quantidade de resposta da questão
+    quantidadeOpcao.innerHTML = `${indiceQuestao + 1} / ${questions.length}`;
+    // pega a pergunta da lista de perguntas no indice indicado
+    const item = questions[indiceQuestao];
+    //pega a pergunta do item
+    perguntas.innerHTML = item.questao;
+    // Caso seja a ultima questão: os botões devem chamar a função de enviar respostas
+    if(indiceQuestao == questions.length - 1){
+        insereUltimosBotoes(item);
+        
+    }else{
+
+        if(indiceQuestao == 0){
+            createButonProx();
+        }else{
+            createButonProx();
+            createButonBack();
+            console.log(indiceQuestao);
+        }
+        //caso contrario
+        insereBotoes(item);
+    
+    }
+}
+
+function insereBotoes(item){
+    // atualiza o indice para valor 1
+    index = 1;
+    // para todas possiveis resposta da pergunta 
+    item.respostas.forEach((resposta) =>{
+        // percorre todas as opções da questão construindo os botões
+        const div = document.createElement("div");
+        div.innerHTML = `
+            <button class="resposta" value="${index}">
+                ${resposta.opcao}
+            </button>
+        `;
+        // atualiza o indice
+        index++;
+        // adiciona o envento de proxima questão
+        div.addEventListener('click', (evento) => proximaQuestao(evento.target.value));
+        // adiciona o botão na tela de opções
+        opcoes.appendChild(div);
+    });
+    // atualiza a imagem de fudo
+    fundo.style.backgroundImage = `url('${images[indiceQuestao]}')`;
+}
+
+function insereUltimosBotoes(item){
+    // atualiza o indice
+    index = 1;
+    // para cada questão da 
+    item.respostas.forEach((resposta) =>{
+        // cria um botão para a opção
+        const div = document.createElement("div");
+        div.innerHTML = `
+            <button class="resposta" value="${index}">
+                ${resposta.opcao}
+            </button>
+        `;
+        index++;
+        // adiciona o envento de enviar resposta
+        div.addEventListener('click', (evento) => enviarResposta(evento.target.value));
+        // adiciona o botão na tela de opções
+        opcoes.appendChild(div);
+    });
+    //imagem de fundo é atualizada
+    fundo.style.backgroundImage = `url('${images[indiceQuestao]}')`;
+}
+
+function createButonBack(){
+    const frame = document.getElementById('frame-button1')
+    frame.innerHTML = `
+        <button id="back" class="botaoNavegador">
+            <div id="back-div1"></div>
+            <div id="back-div2"></div>
+        </button>
+    `
+    document.getElementById('back').addEventListener('click', anteriorQuestao);
+}
+
+function createButonProx(){
+    const frame = document.getElementById('frame-button2');
+    frame.innerHTML = `
+        <button id="prox" class="botaoNavegador">
+            <div id="prox-div1"></div>
+            <div id="prox-div2"></div>
+        </button>
+    `
+    document.getElementById('prox').addEventListener('click', proximaQuestao);
+
+}
+
+function deletButtonsNavegate(){
+    const bt1 = document.getElementById('frame-button1');
+    const bt2 = document.getElementById('frame-button2');
+    bt1.remove()
+    bt2.remove()
+}
---
+++ scripts/questions.js
@@ -0,0 +1,102 @@
+
+const questions = [
+    {
+        questao: "Você já usou cigarro eletrônico?",
+        respostas:[
+            {opcao:"sim"},
+            {opcao:"não"}
+            
+        ]
+    },
+    {
+        questao: "Com qual idade você começou a usuar?",
+        respostas:[
+            {opcao:"Menos de 12 anos"},
+            {opcao:"12 - 14 anos"},
+            {opcao:"15 - 17 anos"},
+            {opcao:"18 - anos ou mais"},
+            {opcao:"Sai fora irmão, eu fiz proerd!"}
+        ]
+    },
+    {
+        questao: "Qual foi o principal motivos que te levaram a usar cigarro eletrônico.",
+        respostas:[
+            {opcao:"curiosidade."},
+            {opcao:"pressão dos amigos."},
+            {opcao:"para relaxar."},
+            {opcao:"para se sentir incluido."},
+            {opcao:"é menos pejudicial."},
+            {opcao: "outro"}
+        ]
+    },
+    {
+        questao: "Quais das seguintes substâncias você acredita que podem esta no cigarro eletrônico?",
+        respostas:[
+            {opcao:"Nicotina"},
+            {opcao:"Metais pesados"},
+            {opcao:"Metais pesados"},
+            {opcao:"Aromas artificiais"},
+            {opcao:"Sei lá, não sou eu que faço!"}
+            
+        ]
+    },
+    {
+        questao: "Quais dos seguintes riscos à saúde você acha que estão associados ao uso de cigarros eletrônicos? Escolhas todos que você concorda!",
+        respostas:[
+            {opcao:"Problemas respiratórios"},
+            {opcao:"Dependencia de nicotina"},
+            {opcao:"Problemas cardiovasculares"},
+            {opcao:"Danos à saúde bucal"},
+            {opcao:"Câncer"},
+            {opcao:"Não sei, nao sou médico!"}
+        ]
+    },
+    {
+        questao: "Como você acha que o uso de cigarro eletrônico pode afetar sua vida escolar?",
+        respostas:[
+            {opcao:"Pode causar problemas de saúde que afetam os estudos"},
+            {opcao:"Pode levar a problemas de disciplina na escola"},
+            {opcao:"Pode afastar amigos que não concordam co o uso"},
+            {opcao:"Não acho que afeta"},
+            {opcao:"Não sei"}
+            
+        ]
+    },
+    {
+        questao: "Em que local da escola você geralmente usa o cigarrp eletrônico?",
+        respostas:[
+            {opcao:"Banheiros"},
+            {opcao:"corredores"},
+            {opcao:"Patio"},
+            {opcao:"Sala de aula"},
+            {opcao:"outro"}
+        ]
+    },
+    {
+        questao: "Os pais concordam com o seu uso de cigarro eletrônico?",
+        respostas:[
+            {opcao:"Sim, foi meu pai que me deu"},
+            {opcao:"Não, meu pai foi comprar e não voltou."}
+        ]
+    },
+    {
+        questao: "Você gostaria de receber mais informações sobre os riscos e danos associados ao uso de cigarros eletrônicos?",
+        respostas:[
+            {opcao:"Sim"},
+            {opcao:"Não, pra quê?"}            
+        ]
+    },
+    {
+        questao: "Após responder esse questionario punk, você considera reduzir o uso de cigarro eletrônico?",
+        respostas:[
+            {opcao:"Sim"},
+            {opcao:"Talvez, vou fumar um enquanto penso"},
+            {opcao:"Não, eu tô bem... cof!.. cof!... cof!.."},
+            {opcao:"Eu não uso, fiz Proerd"}
+            
+        ]
+    }
+
+]
+
+export default questions;
